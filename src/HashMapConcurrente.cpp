9913127528017aa71b4453fbd29f1930db0467b4
Franco Dalmau <franco@pop-os.localdomain>
2020-05-23 11:32:42 -0300

    wip: ejercicio 3

--- src/HashMapConcurrente.cpp
+++ src/HashMapConcurrente.cpp
@@ -39,18 +39,75 @@ hashMapPair HashMapConcurrente::maximo() {
             it.haySiguiente();
             it.avanzar()
         ) {
+            // version original
             if (it.siguiente().second > max->second) {
                 max->first = it.siguiente().first;
                 max->second = it.siguiente().second;
             }
+            /*
+            // ejercicio 3a
+            // hashMapPair *possibleNewMax = new hashMapPair;
+            // possibleNewMax->first = it.siguiente().first;
+            // possibleNewMax->second = it.siguiente().second;
+            
+            // // habría que ver si se puede hacer todo el if atómico
+            // if (possibleNewMax->second > max->second){
+            //     max = possibleNewMax;
+            // }
+            */
         }
     }
 
     return *max;
 }
+/*
+struct thread_args {
+    hashMapPair &max;
+    int (&filas)[HashMapConcurrente::cantLetras];
+};
+
+// Recorre las filas sin ocupar y las marca atómicamente cuando empieza
+void *funcion_thread(void *arg) {
+    thread_args *args_struct = (thread_args *) arg;
+
+    for (unsigned int index = 0; index < HashMapConcurrente::cantLetras; index++) {
+        // Si filas[index] es 1, ya fue tomada y avanzo. Sino la ocupo
+        if (args_struct->filas[index].atomic_compare_exchange_strong(1,1)) break;
+        for (
+            auto it = tabla[index]->crearIt();
+            it.haySiguiente();
+            it.avanzar()
+        ) {
+            if (it.siguiente().second > args_struct->max->second) {
+                args_struct->max->first = it.siguiente().first;
+                args_struct->max->second = it.siguiente().second;
+            }
+        }
+    }
+}
 
 hashMapPair HashMapConcurrente::maximoParalelo(unsigned int cantThreads) {
     // Completar (Ejercicio 3)
+    pthread_t tid[cantThreads];
+
+    // declarar thread_args
+    int filas[HashMapConcurrente::cantLetras] = {0};
+
+    hashMapPair *max = new hashMapPair();
+    max->second = 0;
+
+    thread_args args = {*max, filas};
+
+    for (int i = 0; i < cantThreads; i++){
+        pthread_create(&tid[i], NULL, funcion_thread, &args);
+    }
+
+    for (int i = 0; i < cantThreads; i++){
+        pthread_join(tid[i], NULL);
+    }
+
+    return *max;
 }
+*/
 
 #endif
