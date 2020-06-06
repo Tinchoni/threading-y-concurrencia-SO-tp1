5ebe4abc9d36de7d37b0b031ac1262edd55b7f27
Franco Dalmau <franco@pop-os.localdomain>
2020-06-06 19:42:04 -0300

    experimento max paralelo con promedio de resultados

--- src/exp.cpp
+++ src/exp.cpp
@@ -2,29 +2,52 @@
 #include <iostream>
 #include <time.h>
 #include "./HashMapConcurrente.hpp"
+#include "./CargarArchivos.hpp"
 
 
 int main(int argc, char **argv) {
     struct timespec start, end;
 
-    // inicia reloj
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    std::ios_base::sync_with_stdio(false);
+    HashMapConcurrente* hashMap = new HashMapConcurrente;
+    int cantidad_de_palabras = cargarArchivo(*hashMap, "data/corpus");
 
-    // corre funcion
+    // columnas para el csv
+    std::cout << "cantThreads,tiempo" << std::endl;
 
-    // detiene reloj
-    clock_gettime(CLOCK_MONOTONIC, &end); 
+    for(int cant_Threads = 1; cant_Threads <= 26; cant_Threads++){
+        const int REPETICIONES = 500;
+        double tiempo_promediado = 0;
 
-    // calcula tiempo transcurrido
-    double time_taken; 
-    time_taken = (end.tv_sec - start.tv_sec) * 1e9; 
-    time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9; 
+        //repeticiones para promediar resultado
+        for(int repeticion = 0; repeticion < REPETICIONES; repeticion++){
+        
+        // inicia reloj
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        std::ios_base::sync_with_stdio(false);
 
-    
-    std::cout << "Time taken by program is : " << std::fixed 
-         << time_taken; 
-    std::cout << " sec" << std::endl;
+        // corre funcion
+        hashMap->maximoParalelo(cant_Threads);
+        
+        // detiene reloj
+        clock_gettime(CLOCK_MONOTONIC, &end); 
+
+
+        // calcula tiempo transcurrido
+        double tiempo_transcurrido;
+        tiempo_transcurrido = (end.tv_sec - start.tv_sec) * 1e9; 
+        tiempo_transcurrido = (tiempo_transcurrido + (end.tv_nsec - start.tv_nsec)) * 1e-9; 
+
+        tiempo_promediado += tiempo_transcurrido;
+        
+        }
+
+        tiempo_promediado /= REPETICIONES;
+
+        std::cout << cant_Threads << ",";
+        std::cout << std::fixed 
+            << tiempo_promediado; 
+        std::cout << std::endl;
+    }
 
 	return 0;
 }
